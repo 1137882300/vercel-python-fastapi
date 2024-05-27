5f00fe96c4a31954a707eacfbd82a36c0534dc3d
juzi <zzh@youxiake.com>
2024-05-27 09:49:25 +0800

    add openai proxy

---
+++ api/v1/openai.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import typing
+
+import pydantic
+from fastapi import Header
+from fastapi.routing import APIRouter
+from openai import AsyncClient
+
+router = APIRouter()
+
+
+class ChatArgs(pydantic.BaseModel):
+    model: str
+    messages: typing.List[typing.Dict[str, str]]
+
+
+@router.post("/proxy")
+async def openai_api(args: ChatArgs, authorization: str = Header(...)):
+    api_key = authorization.split(" ")[1]
+    client = AsyncClient(base_url="https://api.openai.com",
+                         api_key=api_key)
+    return await client.chat.completions.create(
+        model=args.model,
+        messages=args.messages,
+    )
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from api.hello import router as hello_router
 from api.random import router as random_router
 from api.v1.groq import router as groq_router
 from api.v1.gemini import router as gemini_router
+from api.v1.openai import router as openai_router
 from fastapi import FastAPI
 from fastapi.responses import Response
 
@@ -13,6 +14,7 @@ app.include_router(hello_router, prefix="/hello")
 app.include_router(random_router, prefix="/random")
 app.include_router(groq_router, prefix="/v1/groq")
 app.include_router(gemini_router, prefix="/v1/gemini")
+app.include_router(openai_router, prefix="/v1/openai")
 
 
 @app.get("/")
